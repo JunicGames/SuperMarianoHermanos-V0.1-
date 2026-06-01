40f9005bcd95a77c928a62ce49db6d84feeac8a5
Juan José Mazariegos <mazariegosjuanjose6@gmail.com>
2026-06-01 06:41:04 -0600

    CHECKPOINT C++ apesta y ya esta la pantalla completa en la terminal pero con el menu traslapado

--- Mario.cpp
+++ Mario.cpp
@@ -1,23 +1,18 @@
-/*
- * mario.cpp - Super Mario Bros. ASCII - Fase 2: Entorno Gráfico (Migrado a C++)
- * Universidad del Valle de Guatemala
- * Programación de Microprocesadores - Primer Ciclo 2026
- *
- * Compilar con: g++ mario.cpp -o mario -lncurses
- * Requiere: ncurses (soporta hilos estándar de C++11)
- */
-
 #include <ncurses/ncurses.h>
 #include <ctime>
 #include <cstdlib>
+#include <iostream> // Necesario para la limpieza
 
 #include "Game.h"
 #include "Menu.h"
 
 int main()
 {
+    // Limpieza agresiva de la terminal antes de iniciar ncurses
+    std::cout << "\033[2J\033[H"; 
+    
     initscr();
-    cbreak();
+    raw(); 
     noecho();
     keypad(stdscr, TRUE);
     curs_set(0);
@@ -26,14 +21,13 @@ int main()
     if (has_colors()) {
         start_color();
         use_default_colors();
-
-        init_pair(1, COLOR_WHITE,  -1);
-        init_pair(2, COLOR_GREEN,  -1);
+        init_pair(1, COLOR_WHITE, -1);
+        init_pair(2, COLOR_GREEN, -1);
         init_pair(3, COLOR_YELLOW, -1);
-        init_pair(4, COLOR_RED,    -1);
-        init_pair(5, COLOR_CYAN,   -1);
-        init_pair(6, COLOR_BLACK,  COLOR_WHITE);
-        init_pair(7, COLOR_BLUE,   -1);
+        init_pair(4, COLOR_RED, -1);
+        init_pair(5, COLOR_CYAN, -1);
+        init_pair(6, COLOR_BLACK, COLOR_WHITE);
+        init_pair(7, COLOR_BLUE, -1);
     }
 
     std::srand(static_cast<unsigned>(std::time(nullptr)));
@@ -43,22 +37,17 @@ int main()
 
     while (true) {
         int opt = menu.menu_screen();
-
         if (opt == 0) {
             int mode = menu.mode_select_screen();
-
             if (mode > 0) {
                 super_mario.set_mode(mode);
                 super_mario.game_loop();
             }
-        }
-        else if (opt == 1) {
+        } else if (opt == 1) {
             menu.instructions_screen();
-        }
-        else if (opt == 2) {
+        } else if (opt == 2) {
             menu.scores_screen();
-        }
-        else {
+        } else {
             break;
         }
     }
--- Renderer.h
+++ Renderer.h
@@ -1,16 +1,19 @@
 #pragma once
 #include <ncurses/ncurses.h>
+#include <string>
+#include <vector>
 
-#include "Constantes.h"
-#include "ScoreManager.h"
-#include "Entidades/MarioPlayer.h"
-#include "Entidades/Coin.h"
-#include "Entidades/Enemy.h"
-#include "Entidades/Projectile.h"
-#include "Level.h"
+// En lugar de incluir los .h de las entidades, usa "Forward Declaration"
+class MarioPlayer;
+class Level;
+class Enemy;
+class Coin;
+class Projectile;
+class ScoreManager; // Si no lo necesitas aquí dentro, quita este también
 
 class Renderer {
 public:
+    // Ahora el compilador sabe que estas clases existen sin tener que cargar todo el archivo .h
     void draw_board(
         const MarioPlayer& mario,
         const Level& level,
@@ -19,13 +22,7 @@ public:
         const std::vector<Projectile>& projectiles
     );
 
-    void draw_hud(
-        const MarioPlayer& mario,
-        int level_time,
-        int game_mode,
-        int hi_score
-    );
-
+    void draw_hud(const MarioPlayer& mario, int level_time, int game_mode, int hi_score);
     void draw_pause();
     void draw_box(int y, int x, int h, int w);
     void draw_centered(int row, const std::string& str);
